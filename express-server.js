2bf9db517fa0dd61381a7f643a23a310f5a14dbc
Max Graham <m.huw.graham@gmail.com>
2019-01-15 18:33:20 +0000

    Added view engine and blank index page.

--- express-server.js
+++ express-server.js
@@ -4,6 +4,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default post 8080
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -20,6 +22,11 @@ app.get('/urls.json', function(request, response){
 
 app.get('/hello', function(request, response){
   response.send('<html><body>Hello <b>World</b></body></html>\n');
+});
+
+app.get('/urls', function(request, response){
+  let pageVariables = { urls: urlDatabase };
+  resonse.render('urls-index', pageVariables);
 });
 
 app.listen(PORT, function(){
