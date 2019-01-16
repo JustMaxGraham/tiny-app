3a65e0ee00f62ec0d80efe5ad906bb820d373f34
Max Graham <m.huw.graham@gmail.com>
2019-01-16 18:17:36 +0000

    Can delete a url from database. Working on editing one.

--- express-server.js
+++ express-server.js
@@ -10,35 +10,38 @@ function generateRandomString() {
   return (randomString);
 }
 
-generateRandomString();
+//generateRandomString();
 
 const express = require("express");
 const app = express();
 const PORT = 8080; //default post 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-
-const urlDatabase = {
+let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// When root page is requested, send the page.
 app.get('/', function(require, response){
   response.send("Hello!");
 
 });
 
+//When /urls.json  is requested, send the databse in JSON format.
 app.get('/urls.json', function(request, response){
   response.json(urlDatabase);
 });
 
+//When /hello is requested, send html code of the page.
 app.get('/hello', function(request, response){
   response.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
+//When /urls is requested, send 'urls-index' page, with data
 app.get('/urls', function(request, response){
   let pageVariables = { urls: urlDatabase };
   response.render('urls-index', pageVariables);
@@ -48,19 +51,41 @@ app.get('/urls/new', function(request, response){
   response.render('urls-new');
 });
 
+//When /urls/:id is requested, response with page with individual url pair.
 app.get('/urls/:id', function(request, response){
   let templateVars = { shortURL: request.params.id, longURL: urlDatabase[request.params.id] };
   response.render('urls-show', templateVars);
 });
 
+app.post('/urls/:id', function(request, resonse){
+  console.log(request.params.id);
+  urlDatabase[request.params.id] = request.body.editURL;
+
+  resonse.render('/urls');
+});
+
+//Create a new tiny url for given long url.
+//Add to database. render new page with the new pair of urls.
 app.post('/urls', function(request, response){
   let newShortURL = generateRandomString();
   urlDatabase[newShortURL] = request.body.longURL;
-  let templateVars = { shortURL: newShortURL, longURL: urlDatabase[newShortURL] };
-
-  response.render('urls-show', templateVars );
+  //let templateVars = { shortURL: newShortURL, longURL: urlDatabase[newShortURL] };
+  let pageVariables = { urls: urlDatabase };
+  response.render('urls-index', pageVariables);
+  //response.render('urls-show', templateVars );
 
 });
+
+app.post('/urls/:tinyUrl/delete', function(request, response){
+  delete urlDatabase[request.params.tinyUrl];
+  //let templateVars = { shortURL: newShortURL, longURL: urlDatabase[newShortURL] };
+  //let pageVariables = { urls: urlDatabase };
+  //response.render('urls-index', pageVariables);
+  //response.render('urls-show', templateVars );
+  response.redirect('/urls');
+
+});
+
 
 app.get("/u/:shortURL", (request, response) => {
   let longURL = urlDatabase[request.params.shortURL];
